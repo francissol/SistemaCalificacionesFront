5f929e40d0842c1a81b6461cf184e533bde7cfc2
francissol <fs2022-3720@uce.edu.do>
2026-06-24 09:59:07 -0400

    Update axios base URL for production environment

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7023/api",
+  baseURL: "https://sistemacalificaciones-production.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
